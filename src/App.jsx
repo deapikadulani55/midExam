707f4562ae263fb03413877227983aac765e6e52
deepikadulani55 <deepikadulani55@gmail.com>
2023-10-17 09:57:50 +0500

    initial table setup with static data

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import sampleData from './sampleData'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
